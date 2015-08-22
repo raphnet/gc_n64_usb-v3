7c991aa1a4026c0d2f625adbe8eee97b7fa2cdca
Raphael Assenat <raph@raphnet.net>
2015-08-22 00:44:01 -0400

    Corrected gamepad button bits definitions

--- gamepads.h
+++ gamepads.h
@@ -15,24 +15,22 @@ typedef struct _n64_pad_data {
 	unsigned char raw_data[N64_RAW_SIZE];
 } n64_pad_data;
 
-#define N64_BTN_A			0x0001
-#define N64_BTN_B			0x0002
-#define N64_BTN_Z			0x0004
-#define N64_BTN_START		0x0008
-#define N64_BTN_DPAD_UP		0x0010
-#define N64_BTN_DPAD_DOWN	0x0020
-#define N64_BTN_DPAD_LEFT	0x0040
-#define N64_BTN_DPAD_RIGHT	0x0080
-
-#define N64_BTN_RSVD_LOW1	0x0100
-#define N64_BTN_RSVD_LOW2	0x0200
-
-#define N64_BTN_L			0x0400
-#define N64_BTN_R			0x0800
-#define N64_BTN_C_UP		0x1000
-#define N64_BTN_C_DOWN		0x2000
-#define N64_BTN_C_LEFT		0x4000
-#define N64_BTN_C_RIGHT		0x8000
+#define N64_BTN_A			0x8000
+#define N64_BTN_B			0x4000
+#define N64_BTN_Z			0x2000
+#define N64_BTN_START		0x1000
+#define N64_BTN_DPAD_UP		0x0800
+#define N64_BTN_DPAD_DOWN	0x0400
+#define N64_BTN_DPAD_LEFT	0x0200
+#define N64_BTN_DPAD_RIGHT	0x0100
+#define N64_BTN_RSVD_LOW1	0x0080
+#define N64_BTN_RSVD_LOW2	0x0040
+#define N64_BTN_L			0x0020
+#define N64_BTN_R			0x0010
+#define N64_BTN_C_UP		0x0008
+#define N64_BTN_C_DOWN		0x0004
+#define N64_BTN_C_LEFT		0x0002
+#define N64_BTN_C_RIGHT		0x0001
 
 
 typedef struct _gc_pad_data {
@@ -42,25 +40,25 @@ typedef struct _gc_pad_data {
 	unsigned char raw_data[GC_RAW_SIZE];
 } gc_pad_data;
 
-#define GC_BTN_RSVD0		0x0001
-#define GC_BTN_RSVD1		0x0002
-#define GC_BTN_RSVD2		0x0004
+#define GC_BTN_A			0x0001
+#define GC_BTN_B			0x0002
+#define GC_BTN_X			0x0004
+#define GC_BTN_Y			0x0008
 
-#define GC_BTN_START		0x0008
-#define GC_BTN_Y			0x0010
-#define GC_BTN_X			0x0020
-#define GC_BTN_B			0x0040
-#define GC_BTN_A			0x0080
+#define GC_BTN_START		0x0010
+#define GC_BTN_RSVD0		0x0020
+#define GC_BTN_RSVD1		0x0040
+#define GC_BTN_RSVD2		0x0080
 
-#define GC_BTN_RSVD3		0x0100
+#define GC_BTN_DPAD_LEFT	0x0100
+#define GC_BTN_DPAD_RIGHT	0x0200
+#define GC_BTN_DPAD_DOWN	0x0400
+#define GC_BTN_DPAD_UP		0x0800
 
-#define GC_BTN_L			0x0200
-#define GC_BTN_R			0x0400
-#define GC_BTN_Z			0x0800
-#define GC_BTN_DPAD_UP		0x1000
-#define GC_BTN_DPAD_DOWN	0x2000
-#define GC_BTN_DPAD_RIGHT	0x4000
-#define GC_BTN_DPAD_LEFT	0x8000
+#define GC_BTN_Z			0x1000
+#define GC_BTN_R			0x2000
+#define GC_BTN_L			0x4000
+#define GC_BTN_RSVD3		0x8000
 
 #define GC_ALL_BUTTONS      (GC_BTN_START|GC_BTN_Y|GC_BTN_X|GC_BTN_B|GC_BTN_A|GC_BTN_L|GC_BTN_R|GC_BTN_Z|GC_BTN_DPAD_UP|GC_BTN_DPAD_DOWN|GC_BTN_DPAD_RIGHT|GC_BTN_DPAD_LEFT)
 
