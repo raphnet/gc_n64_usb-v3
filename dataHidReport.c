90895b0972bee089208fa8e2adbbe691fee8fc13
Raphael Assenat <raph@raphnet.net>
2015-10-24 14:17:42 -0400

    Allocate 40 bytes for custom commands

--- dataHidReport.c
+++ dataHidReport.c
@@ -8,7 +8,7 @@ const uint8_t dataHidReport[] PROGMEM = {
 	0x15, 0x00,         //   LOGICAL_MINIMUM (0)
 	0x26, 0xff, 0x00,   //   LOGICAL_MAXIMUM (255)
 	0x75, 0x08,         //   REPORT_SIZE (8)
-	0x95, 0x05,         //   REPORT_COUNT (5)
+	0x95, 40,         //   REPORT_COUNT (40)
 	0x09, 0x01,         //   USAGE (Vendor defined)
 	0xB1, 0x00,         //   FEATURE (Data,Ary,Abs)
 	0xc0                // END_COLLECTION
