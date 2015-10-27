3836cc2a6e380f06e2c3e95fb9d02861a6ab77fc
Raphael Assenat <raph@raphnet.net>
2015-10-26 23:33:39 -0400

    Use different signature for stk525 build

--- version.c
+++ version.c
@@ -1,5 +1,10 @@
 #include <avr/pgmspace.h>
 #include "version.h"
 
+
 const char *g_version = VERSIONSTR; // From Makefile
-const char signature[] PROGMEM = "9c3ea8b8-753f-11e5-a0dc-001bfca3c593";
+#ifdef STK525
+const char g_signature[] PROGMEM = "e106420a-7c54-11e5-ae9a-001bfca3c593";
+#else
+const char g_signature[] PROGMEM = "9c3ea8b8-753f-11e5-a0dc-001bfca3c593";
+#endif
--- version.h
+++ version.h
@@ -1,6 +1,9 @@
 #ifndef _version_h__
 #define _version_h__
 
+#include <avr/pgmspace.h>
+
 extern const char *g_version;
+extern const char g_signature[] PROGMEM;
 
 #endif // _version_h__
