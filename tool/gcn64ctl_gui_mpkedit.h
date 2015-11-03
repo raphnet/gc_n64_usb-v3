452e324d07e4d8fe0d44956f0528235f51ff909f
Raphael Assenat <raph@raphnet.net>
2015-11-02 21:53:35 -0500

    Add read n64 mempak support

--- tool/gcn64ctl_gui_mpkedit.h
+++ tool/gcn64ctl_gui_mpkedit.h
@@ -1,10 +1,13 @@
 #ifndef gcn64ctl_gui_mpkedit_h__
 #define gcn64ctl_gui_mpkedit_h__
 
+#include "mempak.h"
+
 struct mpkedit_data;
 struct application;
 
 struct mpkedit_data *mpkedit_new(struct application *app);
 void mpkedit_free(struct mpkedit_data *mpke);
+void mpke_replaceMpk(struct application *app, mempak_structure_t *mpk, char *filename);
 
 #endif
