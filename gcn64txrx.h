8fe9a98a112d055a2989f2f510874d0fbb071a37
Raphael Assenat <raph@raphnet.net>
2015-12-06 16:11:29 -0500

    gcn64txrx: Return an error code specific to overflow

--- gcn64txrx.h
+++ gcn64txrx.h
@@ -7,7 +7,7 @@ void gcn64_sendBytes(const unsigned char *data, unsigned char n_bytes);
  * \brief Receive up to \max_bytes bytes
  * \param dstbuf Destination buffer
  * \param max_bytes The maximum number of bytes
- * \return The number of received bytes. 0xFF in case of error
+ * \return The number of received bytes. 0xFF in case of error, 0xFE in case of overflow (max_bytes too small)
  */
 unsigned char gcn64_receiveBytes(unsigned char *dstbuf, unsigned char max_bytes);
 
