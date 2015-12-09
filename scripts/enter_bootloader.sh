c9f45b7fe8317b5b99c0b7b5a3de2e078b8bb474
Raphael Assenat <raph@raphnet.net>
2015-12-08 22:08:13 -0500

    Fix tool -> tools

--- scripts/enter_bootloader.sh
+++ scripts/enter_bootloader.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TOOL=./tool/gcn64ctl
+TOOL=./tools/gcn64ctl
 
 if [ -x $TOOL ]; then
 	$TOOL -f --bootloader
-	exit 0 # The tool fails, but we should continue
+	exit 0 # The tool fails, but we should continue anyway in case the chip is already in bootloader
 else
 	echo $TOOL not found. Please compile it to enter bootloader automatically.
 fi
