3ec1c0b711e741bd3bd5a9cb890de6f79c8fde36
Raphael Assenat <raph@raphnet.net>
2016-05-22 17:33:26 -0400

    Version 3.2 strings and PID

--- usbstrings.c
+++ usbstrings.c
@@ -19,7 +19,7 @@
 
 const wchar_t *g_usb_strings[] = {
 	[0] = L"raphnet technologies", 	// 1 : Vendor
-	[1] = L"GC/N64 to USB v3.0",	// 2: Product
+	[1] = L"GC/N64 to USB v3.2",	// 2: Product
 	[2] = L"123456",				// 3 : Serial
 };
 
